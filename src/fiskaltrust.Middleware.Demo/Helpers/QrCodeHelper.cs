0b2cb4a71279f2d3e1d09fae8c633ed7899fcdc6
Stefan Kert <stefan.kert@sbg.at>
2020-02-04 17:41:57 +0100

    Added Middleware-DE demo

---
+++ src/fiskaltrust.Middleware.Demo/Helpers/QrCodeHelper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using fiskaltrust.ifPOS.v1;
+
+namespace fiskaltrust.Middleware.Demo
+{
+    public static class QrCodeHelper
+    {
+        public static void PrintQrCode(SignaturItem signaturItem)
+        {
+            var width = 128;
+            var ft = QRTextChars(signaturItem.Data, width, true);
+
+            var line = 0;
+            Console.WriteLine(signaturItem.Data);
+            Console.WriteLine("============================================================");
+            while (line * width < ft.Length)
+            {
+                Console.WriteLine(ft, width * line++, width);
+            }
+            Console.WriteLine("============================================================");
+        }
+
+        public static char[] QRTextChars(string data, int width = 64, bool invers = false)
+        {
+            var bytes = QRTextBytes(data, width, invers);
+
+            var chars = new char[bytes.Length];
+
+            var line = 0;
+            while (bytes.Length > line * width)
+            {
+                try
+                {
+                    for (var i = 0; i < width; i++)
+                    {
+                        var pos = (line * width) + i;
+                        if (bytes[pos] == 0xDB)
+                        {
+                            chars[pos] = (char)0x2588;
+                        }
+                        else if (bytes[pos] == 0xDC)
+                        {
+                            chars[pos] = (char)0x2584;
+                        }
+                        else if (bytes[pos] == 0xDF)
+                        {
+                            chars[pos] = (char)0x2580;
+                        }
+                        else
+                        {
+                            chars[pos] = (char)0x20;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                }
+                line++;
+            }
+            return chars;
+        }
+
+        public static byte[] QRTextBytes(string data, int width = 64, bool invers = false)
+        {
+            var writer = new ZXing.QrCode.QRCodeWriter();
+
+            var hints = new Dictionary<ZXing.EncodeHintType, object>
+            {
+                { ZXing.EncodeHintType.CHARACTER_SET, "ISO-8859-1" },
+                { ZXing.EncodeHintType.ERROR_CORRECTION, ZXing.QrCode.Internal.ErrorCorrectionLevel.M }
+            };
+
+            var matrix = writer.encode(data, ZXing.BarcodeFormat.QR_CODE, width, width, hints);
+            var bytes = new List<byte>();
+            var y = 0;
+            while (y < width)
+            {
+
+                for (var x = 0; x < width; x++)
+                {
+                    if (matrix[x, y] == true && matrix[x, y + 1] == true)
+                    {
+                        bytes.Add(219);
+                    }
+                    else if (matrix[x, y] == true && matrix[x, y + 1] == false)
+                    {
+                        bytes.Add(223);
+                    }
+                    else if (matrix[x, y] == false && matrix[x, y + 1] == true)
+                    {
+                        bytes.Add(220);
+                    }
+                    else
+                    {
+                        bytes.Add(32);
+                    }
+                }
+                y += 2;
+            }
+
+            var result = bytes.ToArray();
+
+            if (invers)
+            {
+                for (var i = 0; i < result.Length; i++)
+                {
+                    if (result[i] == 0xDB)
+                    {
+                        result[i] = 0x20;
+                    }
+                    else if (result[i] == 0x20)
+                    {
+                        result[i] = 0xDB;
+                    }
+                    else if (result[i] == 0xDC)
+                    {
+                        result[i] = 0xDF;
+                    }
+                    else if (result[i] == 0xDF)
+                    {
+                        result[i] = 0xDC;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/fiskaltrust.Middleware.Demo/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using fiskaltrust.ifPOS.v1;
+using fiskaltrust.Middleware.Demo.Helpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace fiskaltrust.Middleware.Demo
+{
+    public static class Program
+    {
+        private static IPOS pos = null;
+        private static Guid cashBoxId;
+        private static readonly Dictionary<string, ReceiptRequest> Examples = new Dictionary<string, ReceiptRequest>();
+
+        /// <param name="cashboxId">The cashboxid for the Middleware.</param>
+        /// <param name="url">The url that is used for sending requests to the Middleware(Default: grpc://localhost:10103).</param>
+        /// <returns></returns>
+        public static async Task Main(string cashboxId, string url = "grpc://localhost:10103")
+        {
+            try
+            {
+                if (Guid.TryParse(cashboxId, out var parsedCashBoxId))
+                {
+                    cashBoxId = parsedCashBoxId;
+                    LoadExamples();
+                    pos = GetPosClientForUrl(url);
+                    await EchoAsync();
+
+
+                    while (true)
+                    {
+                        await MenuAsync();
+                    }
+                }
+                else
+                {
+                    ConsoleHelper.WriteError("Please provide a valid CashBoxId");
+                }
+            }
+            catch (Exception ex)
+            {
+                ConsoleHelper.WriteError(ex.ToString());
+
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static void LoadExamples()
+        {
+            foreach (var folder in Directory.GetDirectories("ReceiptExamples"))
+            {
+                var content = File.ReadAllText(Path.Combine(folder, "request.json"));
+                var req = JsonConvert.DeserializeObject<ReceiptRequest>(content);
+                Examples.Add(Path.GetFileName(folder), req);
+            }
+        }
+
+        private static IPOS GetPosClientForUrl(string url)
+        {
+            if (url.StartsWith("grpc://"))
+            {
+                var uri = new Uri(url);
+                return GrpcHelper.GetClient<IPOS>(uri.Host, uri.Port);
+            }
+            else
+            {
+#if WCF
+                    return WcfHelper.GetClient<IPOS>(url);
+#else
+                throw new NotSupportedException($"The url {url} is not supported in .NET Core. Please provide a valid one. If you want to use WCF for connection make sure you are running the net461 version of this application.");
+#endif
+            }
+        }
+
+        private static async Task EchoAsync()
+        {
+            try
+            {
+                var message = await pos.EchoAsync(new EchoRequest
+                {
+                    Message = "message"
+                });
+                if (message.Message != "message")
+                {
+                    throw new Exception("echo failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        private static async Task MenuAsync()
+        {
+            PrintOptions();
+            var input = Console.ReadLine();
+            if (!int.TryParse(input, out var inputInt) || inputInt > Examples.Keys.Count)
+            {
+                Console.WriteLine($"\"{input}\" nicht erkannt.");
+            }
+            else
+            {
+                var req = Examples.Values.ToList()[inputInt - 1];
+                await PerformPosRequest(req);
+                Console.WriteLine("Please press enter to continue.");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            await MenuAsync();
+        }
+
+        private static void PrintOptions()
+        {
+            var i = 1;
+            foreach (var example in Examples)
+            {
+                Console.WriteLine($"{i}: {example.Key} - ({example.Value.ftReceiptCase:X})");
+                i++;
+            }
+            Console.WriteLine("exit: Program beenden");
+        }
+
+        private static async Task PerformPosRequest(ReceiptRequest req)
+        {
+            try
+            {
+                PrintRequest(req);
+                req.ftCashBoxID = cashBoxId.ToString();
+                var resp = await pos.SignAsync(req);
+                PrintResponse(resp);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("An error occured when trying to send the request.");
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        private static void PrintRequest(ReceiptRequest receiptRequest)
+        {
+            Console.WriteLine("{0:G} ReceiptRequest:", DateTime.Now);
+            Console.WriteLine(JsonConvert.SerializeObject(receiptRequest, Formatting.Indented));
+        }
+
+        private static void PrintResponse(ReceiptResponse data)
+        {
+            if (data != null)
+            {
+                Console.WriteLine("{0:G} ReceiptResponse:", DateTime.Now);
+                Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+                Console.WriteLine("========== n: {0} CashBoxIdentificateion:{1} ReceiptIdentification:{2} ==========", data.cbReceiptReference, data.ftCashBoxIdentification, data.ftReceiptIdentification);
+                foreach (var item in data.ftSignatures.Where(x => x.ftSignatureFormat != 0x03))
+                {
+                    Console.WriteLine("{0}:{1}", item.Caption, item.Data);
+                }
+                foreach (var item in data.ftSignatures.Where(x => x.ftSignatureFormat == 0x03))
+                {
+                    QrCodeHelper.PrintQrCode(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("null-result!!!");
+            }
+        }
+    }
+}
